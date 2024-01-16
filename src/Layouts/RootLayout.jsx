9c516a32e757bc487d62426b71f0a37d9b81a90e
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-01-16 21:03:37 +0600

    deploy problem

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-import NavBar from "./Components/NavBar";
+import NavBar from "../Components/Navbar";
 import NavbarHeader from "../Components/NavbarHeader";
 import Footer from "../Components/Footer";
 
