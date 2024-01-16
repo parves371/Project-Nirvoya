9290a5e06164c5bf1c5a7bf8f96e7bad0258b1b7
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-01-16 20:57:26 +0600

    bug

--- src/Layouts/RootLayout.jsx
+++ src/Layouts/RootLayout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 
-
 import NavBar from "../Components/NavBar";
 import NavbarHeader from "../Components/NavbarHeader";
 import Footer from "../Components/Footer";
