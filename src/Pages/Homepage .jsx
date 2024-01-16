48235c9f31e9307adf90eb445fa2b74bfe97a7ff
MD. Parves Ahmed Shuvo <152497490+parves371@users.noreply.github.com>
2024-01-16 21:25:51 +0600

    i think i found the problem

--- src/Pages/Homepage .jsx
+++ src/Pages/Homepage .jsx
@@ -1,24 +1,17 @@
 import React from "react";
-import Navbar from "../Components/NavBar";
-import NavbarHeader from "../Components/NavbarHeader";
+
 import Banner from "../Components/Banner";
 import Category from "../Components/Category";
 import FlashDeals from "../Components/FlashDeals";
 import FeaturedProduct from "../Components/FeaturedProduct";
-import Footer from "../Components/Footer";
-
 
 const Homepage = () => {
   return (
     <>
-      {/* <Navbar /> */}
-      {/* <NavbarHeader /> */}
-      <Banner/>
-      <Category/>
-      <FlashDeals/>
-      <FeaturedProduct/>
-      {/* <Footer/> */}
-
+      <Banner />
+      <Category />
+      <FlashDeals />
+      <FeaturedProduct />
     </>
   );
 };
